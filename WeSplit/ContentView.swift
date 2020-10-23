d39c3ed6afbb03e887ab503cdc2036ccdb273f0a
Abdul Khan <akhan@lcs.on.ca>
2020-10-23 13:00:34 -0400

    Played around and created textfield for Amount, also a picker for number of people.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount = ""
+    let numberOfPeople = ["2","3","4","5","6","7","8"]
+    @State private var selectedNumberOfPeople = 2
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Check Amount", text: $amount)
+                    Text("Check amount is $\(amount)")
+                }
+                Section {
+                    Picker("# of People", selection: $selectedNumberOfPeople) {
+                        ForEach(0 ..< numberOfPeople.count) {
+                            Text(self.numberOfPeople[$0])
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle(Text("WeSplit"))
+        }
     }
 }
 

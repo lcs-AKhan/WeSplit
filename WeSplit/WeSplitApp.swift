b2e2af19e22d7c2ae740517e12855fa8a282aebb
Abdul Khan <akhan@lcs.on.ca>
2020-10-23 11:46:03 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Abdul Ahad Khan on 2020-10-23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
